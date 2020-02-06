36eef0c9368093d18328a3ddc58ad29e5698f947
Holly Phelps <Hollyphelps1992@gmail.com>
2020-02-05 21:30:11 -0800

    creation of server.js routes folder

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+//creating an express server
+var app = express();
+
+var PORT = process.env.PORT || 8040;
+
+//help with data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//point our server to a series of "route" files 
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//start server
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
